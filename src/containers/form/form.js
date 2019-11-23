dc08b6ebe4cd32efdb41a154eb42bb3c6fa71e76
lakshay <ihybrid11@gmail.com>
2019-11-24 02:36:33 +0530

    added required validaiton

--- src/containers/form/form.js
+++ src/containers/form/form.js
@@ -4,12 +4,78 @@ import DIV from '../../components/div/div'
 import classes from './form.css';
 
 class Form extends Component {
+
+    formUserName = '';
+    formUserPassword = '';
+    formState = false;
+
     state = {
-        form: [],
+        form: {
+            username: '',
+            usernameDirty: false,
+            password: '',
+            passwordDirty: false,
+        },
+        error: '',
+        submitDisable: true
     }
 
+    componentDidMount = () => {
+        // console.log('component did mount and state is = ' + JSON.stringify(this.state.form));
+
+    }
+
+    inputOnChangeHandler = (event) => {
+        // if (event.target.value === '') {
+        //     console.log('value = ' + true);
+
+        // }
+        if (event.target.name === 'username') {
+            this.formUserName = event.target.value;
+            this.setState({ usernameDirty: true });
+        } else {
+            this.formUserPassword = event.target.value;
+            this.setState({ passwordDirty: true });
+        }
+
+        const disableSubmit = (this.formUserName !== '' && this.formUserPassword !== '') ? false : true;
+
+        this.setState({ submitDisable: disableSubmit });
+
+
+    }
+
+    submitClickHandler = (event) => {
+
+        event.preventDefault();
+        this.setState({
+            form: {
+                username: this.formUserName,
+                password: this.formUserPassword
+            }
+        });
+
+        console.log(this.state);
+    }
+
+    resetClickHandler = (event) => {
+        this.setState(this.getInitialState());
+        // this.formState = !this.formState;
+    }
 
     render() {
+        const showError = (this.formUserName === '' && this.state.usernameDirty) ? (
+            <span>
+                <p className="errorText" >REQUIRED</p>
+            </span>
+        ) : null;
+
+        const showErrorPass = (this.formUserPassword === '' && this.state.passwordDirty) ? (
+            <span>
+                <p className="errorText" >REQUIRED</p>
+            </span>
+        ) : null;
+
         return (
             <form className="formContainer">
 
@@ -18,16 +84,37 @@ class Form extends Component {
                 </DIV>
                 <DIV className="formContent">
                     <DIV className="formInputGroup">
-                        <label for="username">UserName</label>
-                        <input type="text" name="username"></input>
+                        <DIV>
+                            <label htmlFor="username">UserName</label>
+                            {showError}
+                        </DIV>
+
+
+                        <input
+                            type="text"
+                            name="username"
+                            onBlur={this.inputOnChangeHandler}
+                            required
+                        ></input>
+
                     </DIV>
                     <DIV className="formInputGroup">
-                        <label for="password">Password&nbsp;</label>
-                        <input type="password" name="password"></input>
+                        <DIV>
+                            <label htmlFor="password">Password&nbsp;</label>
+                            {showErrorPass}
+                        </DIV>
+
+                        <input
+                            type="password"
+                            name="password"
+
+                            onBlur={this.inputOnChangeHandler}
+                            required
+                        ></input>
                     </DIV>
                     <DIV className="formInputGroup formButtons">
-                        <button>SUBMIT</button>
-                        <button>RESET</button>
+                        <button onClick={this.submitClickHandler} disabled={this.state.submitDisable}>SUBMIT</button>
+                        <button onClick={this.resetClickHandler}>RESET</button>
                     </DIV>
                 </DIV>
 
