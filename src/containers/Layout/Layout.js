ec3886ef55c9f064ea1c2699a2a8fb52b09e6d1c
lakshay <ihybrid11@gmail.com>
2019-11-23 14:45:22 +0530

    form design

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -5,52 +5,27 @@ import Input from '../../components/UI/Input/Input';
 import classes from './Layout.css';
 
 const Layout = () => (
-    <>
-        <div className="row parentDiv">
-            <h1>React Boilerplate</h1>
-            <p>Welcome to my basic React boilerplate. Thanks for cloning it.</p>
-            <br />
-            <div className="span-1-of-2 divLeft">
-                <h4>Boilerplate Features -</h4>
-                <br />
-                <ul>
-                    <li>Basic UI elements are available
-                    <ol>
-                            <li>Anchor</li>
-                            <li>Button</li>
-                            <li>Ion Icons</li>
-                            <li>Image</li>
-                            <li>Input</li>
-                        </ol>
-                    </li>
-                    <li>Higher Order Wrapping component ready to use</li>
-                    <li>GRID CSS</li>
-                    <li>ION ICONS</li>
-                    <li>ANIMATE CSS</li>
-                    <li>NORMALISE CSS</li>
-                    <li>Media queries</li>
-                    <li>Basic WEBPACK config</li>
-                    <li>LATO Google Font</li>
-
-                </ul>
+    <div className="layout">
+        <div className="formContainer">
+            <div className="appBar">
+                <p>EXAMPLE FORM</p>
             </div>
-            <div className="span-1-of-2 divRight">
-                <h4>Instructions</h4>
-                <br />
-                <ul>
-                    <li>use command npm install to install all the dependencies if you already haven't</li>
-                    <li>npm start to run</li>
-                    <li>npm build to get the production build</li>
-                    <li>
-                        <strong>
-                            Remove all the code from this Layout component and start adding your code here.
-                        </strong>
-                    </li>
-                </ul>
-            </div>
-
+            <form className="formContent">
+                <div className="formInputGroup">
+                    <label for="username">UserName</label>
+                    <input type="text" name="username"></input>
+                </div>
+                <div className="formInputGroup">
+                    <label for="password">Password&nbsp;</label>
+                    <input type="password" name="password"></input>
+                </div>
+                <div className="formInputGroup formButtons">
+                    <button>SUBMIT</button>
+                    <button>RESET</button>
+                </div>
+            </form>
         </div>
-    </>
+    </div>
 );
 
 export default Layout;
